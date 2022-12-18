348b8a1e4f720f78bc0fb8e49de56344f0ec6ba1
jmjmjames <chungjm0711@gmail.com>
2022-12-18 22:46:59 +0900

    chore: 초기 프로젝트 설정

---
+++ src/main/java/com/paywith/PayWithApplication.java
@@ -0,0 +1,13 @@
+package com.paywith;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayWithApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PayWithApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/paywith/PayWithApplicationTests.java
@@ -0,0 +1,13 @@
+package com.paywith;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayWithApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
